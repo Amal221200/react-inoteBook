8ee3751ae3f8072c7635fe35e1f734ad8155104a
Amal221200 <amalmurikkoli2000@gmail.com>
2022-02-14 11:32:10 +0530

    Connected to database

---
+++ backend/index.js
@@ -0,0 +1,14 @@
+const connectToMongoose =  require("./db");
+const express = require('express');
+const app = express();
+const port = 3000;
+
+connectToMongoose();
+
+app.get('/', (req, res)=>{
+    res.send('Hello World!');
+})
+
+app.listen(port, ()=>{
+    console.log('App listening at port '+port)
+})
